1cbc15cc59b20f094e0dd55b6d320a7ca7abc6a2
reset0514 <146703959+reset0514@users.noreply.github.com>
2024-07-22 10:03:14 +0800

    Add files via upload

---
+++ src/pages/ContentScripts/features/repo-community-racing-bar/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, Container } from 'react-dom';
+import $ from 'jquery';
+
+import features from '../../../../feature-manager';
+import isPerceptor from '../../../../helpers/is-perceptor';
+import {
+  getRepoName,
+  isPublicRepoWithMeta,
+} from '../../../../helpers/get-repo-info';
+import { getActivityDetails } from '../../../../api/repo';
+import View from './view';
+import DataNotFound from '../repo-networks/DataNotFound';
+import { RepoActivityDetails } from './data';
+
+const featureId = features.getFeatureID(import.meta.url);
+let repoName: string;
+let repoActivityDetails: RepoActivityDetails;
+
+const getData = async () => {
+  repoActivityDetails = await getActivityDetails(repoName);
+};
+
+const renderTo = (container: Container) => {
+  if (!repoActivityDetails) {
+    render(<DataNotFound />, container);
+    return;
+  }
+  render(
+    <View currentRepo={repoName} repoActivityDetails={repoActivityDetails} />,
+    container
+  );
+};
+
+const init = async (): Promise<void> => {
+  repoName = getRepoName();
+  await getData();
+  const container = document.createElement('div');
+  container.id = featureId;
+  // append before render so that the container has computed width
+  $('#hypercrx-perceptor-slot-repo-activity-racing-bar').append(container);
+  renderTo(container);
+};
+
+const restore = async () => {
+  // Clicking another repo link in one repo will trigger a turbo:visit,
+  // so in a restoration visit we should be careful of the current repo.
+  if (repoName !== getRepoName()) {
+    repoName = getRepoName();
+  }
+  // rerender the chart or it will be empty
+  renderTo($(`#${featureId}`)[0]);
+};
+
+features.add(featureId, {
+  asLongAs: [isPerceptor, isPublicRepoWithMeta],
+  awaitDomReady: false,
+  init,
+  restore,
+});
